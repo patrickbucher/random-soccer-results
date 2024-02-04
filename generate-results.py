2c3a7cb75b6c0d06389ff8b3188d33b8110a623b
Patrick Bucher <patrick.bucher@sluz.ch>
2024-02-04 09:35:45 +0100

    output results to files; play proper number of rounds; generate higher random amount of goals

--- generate-results.py
+++ generate-results.py
@@ -2,6 +2,7 @@
 
 import os
 import random
+import shutil
 import sys
 
 
@@ -18,7 +19,7 @@ def pair_up(teams):
     head = teams[0]
     tail = teams[1:]
     pairings = []
-    for i in range(n):
+    for i in range(n-1):
         whole = [head] + tail
         pairs = list(zip(whole[:m], whole[m:]))
         tail = tail[1:] + [tail[0]]
@@ -34,34 +35,44 @@ def reverse_pairs(pairings):
 
 
 def rand_goals(strength):
-    number = int(random.normalvariate(mu=strength, sigma=1.0))
+    number = int(random.normalvariate(mu=strength, sigma=1.5))
     if number < 0:
         number = 0
     return number
 
 
 def generate_results(teams_dir, results_dir):
+    if not os.path.exists(results_dir):
+        os.mkdir(out_dir)
+
     team_files = os.listdir(teams_dir)
     teams_by_file = {f: read_lines(os.path.join(teams_dir, f))
                      for f in team_files}
+
     for file, teams in teams_by_file.items():
+        out_dir = os.path.join(results_dir, file.split('.')[0])
+        if os.path.exists(out_dir):
+            shutil.rmtree(out_dir)
+        os.mkdir(out_dir)
+
         n = len(teams)
         strength = map(lambda x: 1 + x / n, reversed(range(0, n)))
         team_strengths = dict(zip(teams, strength))
-        out_dir = os.path.join(results_dir, file.split('.')[0])
         first_leg = pair_up(teams)
         second_leg = reverse_pairs(first_leg)
         rounds = first_leg + second_leg
 
-        assert len(rounds) == 2 * n
+        assert len(rounds) == 2 * (n - 1)
         assert all(map(lambda r: len(set(r)) == len(r), rounds))
-        for r in rounds:
-            for p in r:
-                home, away = p[0], p[1]
-                home_s, away_s = team_strengths[home], team_strengths[away]
-                home_g, away_g = rand_goals(home_s), rand_goals(away_s)
-                print('%s %d:%d %s' % (home, home_g, away_g, away))
-                # TODO: output in individual round files
+
+        for (i, r) in enumerate(rounds):
+            round_file = os.path.join(out_dir, f'day{i+1:02d}.txt')
+            with open(round_file, 'w') as f:
+                for p in r:
+                    home, away = p[0], p[1]
+                    home_s, away_s = team_strengths[home], team_strengths[away]
+                    home_g, away_g = rand_goals(home_s), rand_goals(away_s)
+                    f.write('%s %d:%d %s\n' % (home, home_g, away_g, away))
 
 
 if __name__ == '__main__':
